af4fa05350aa58fb6fa995feecd84d1268a72cc1
lschult <136801449+lschult@users.noreply.github.com>
2023-08-20 17:16:40 +0200

    chore: custom loader added

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,22 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-    entry: './src/client/index.js',
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    }
-}
+  entry: "./src/client/index.js",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: "json-loader",
+      },
+    ],
+  },
+};
